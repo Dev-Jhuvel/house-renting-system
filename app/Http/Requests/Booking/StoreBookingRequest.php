e8d8812037494910e423b15e1ef75f02a2401617
Dev-Jhuvel <devjhuvel@gmail.com>
2026-07-19 16:25:30 +0800

    adjust StoreBookingRequest

--- app/Http/Requests/Booking/StoreBookingRequest.php
+++ app/Http/Requests/Booking/StoreBookingRequest.php
@@ -25,7 +25,7 @@ class StoreBookingRequest extends FormRequest
     public function rules(): array
     {
          return [
-            'tenant_id'     => ['required','uuid'],
+            'tenant_id'     => ['required','uuid', new OwnedByAuthUser(Tenant::class)],
             'room_id'       => ['required','uuid', new OwnedByAuthUser(Room::class)],
             'move_in_date'  => ['required','date'],
             'move_out_date' => ['nullable','date', 'after:move_in_date'],
